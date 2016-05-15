a71f1dfa9f4310981e8c1471b449fb9526faa572
jack <jack@jackdemacbook-air.local>
2016-05-15 22:54:02 +0800

    Initial Commit

---
+++ C++ Document/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Document
+//
+//  Created by jack on 16/5/15.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
